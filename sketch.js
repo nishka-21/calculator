742946574fa7d8652c40c682301435d9bdc51393
nishka-21 <85877279+nishka-21@users.noreply.github.com>
2021-06-15 17:42:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,56 @@
-var n1,n2;
-var b1,b2,b3,b4;
+var btn_red;
+var btn_green;
+var btn_blue;
+var btn_yellow;
+
+var r = 0;
+var g = 0;
+var b = 0;
 
 function setup() {
-  
-  createCanvas(400, 400);
-  
-  num1 = createInput();
-  num1.position(5, 60);
-  
-  num2 = createInput();
-  num2.position(200, 60);
-  
-  b1 = createButton("ADD");
-  b1.position(10, 200);
-  b1.mousePressed(add);
-  
-  //b2 = createButton("Multiply");
-  //b2.position(100, 200);
-  //b2.mousePressed(mul);
-  
-  
-  textAlign(CENTER);
-  textSize(15);
+  createCanvas(400, 400);  
+  btn_red=createButton("RED");
+  btn_red.position(120,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green=createButton("GREEN");
+  btn_green.position(200,50);
+  btn_green.mousePressed(green_bg);
+
+  btn_blue=createButton("BLUE");
+  btn_blue.position(300,50);
+  btn_blue.mousePressed(blue_bg);
+
+  btn_pink=createButton("YELLOW");
+  btn_pink.position(20,50);
+  btn_pink.mousePressed(yellow_bg);
+
 }
 
 function draw() {
-  background(200,10,200)
-  text("Number 1",70,50)
-  text("Number 2",270,50)
- 
-  n1 = parseInt (num1.value());
-  n2=parseInt(num2.value());
+  background(r,g,b);
 }
-function add()
+function red_bg()
 {
-  console.log(n1+n2);
+  r=225;
+  g=0;
+  b=0;
 }
-
+function green_bg()
+{
+  r=0;
+  g=225;
+  b=0;
+}
+function blue_bg()
+{
+  r=0;
+  g=0;
+  b=225;
+}
+function yellow_bg()
+{
+r=225;
+g=225;
+b=0;
+}
